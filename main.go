af951c23833d091da384d4d0cdccb771d36adafe
Kalin Staykov <k.t.staykov@gmail.com>
2022-04-16 21:14:38 +0300

    add tarot

--- main.go
+++ main.go
@@ -2,4 +2,5 @@ package main
 
 func main() {
 	println("Based on Moon's " + MoonData[3].MoonPhase + " phase and " + MoonData[3].Visibility + " visibility...")
+	println("Seems like you got " + TarotData[17].Name + " which is of the " + TarotData[17].Arcana)
 }
