9bd7baf106fe9bce7ab91b5d0d194eeb9413a968
Kalin Staykov <k.t.staykov@gmail.com>
2022-04-17 12:01:26 +0300

    handle input from stdin

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"strings"
 	"time"
 )
 
@@ -15,11 +19,32 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
+func readInput() (string, error) {
+	var reader = bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your question: ")
+	_, err := fmt.Fscan(reader)
+	if err != nil {
+		return "", err
+	}
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	text = strings.TrimSuffix(text, "\n")
+	return text, nil
+}
+
 func main() {
+	input, err := readInput()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf(">>> %s <<<\n", input)
 	pickedMoonData := rand.Intn(MoonCount)
 	pickedTarotData := rand.Intn(TarotCount)
 	fmt.Println("Projecting point in time..")
-	println("Moon phase is " + MoonData[pickedMoonData].MoonPhase + " with " + MoonData[pickedMoonData].Visibility + " visibility...")
-	println("Picked " + TarotData[pickedTarotData].Name + " tarot card of the " + TarotData[pickedTarotData].Arcana + "...")
+	fmt.Println("Calibrating universal truth.. 42")
+	fmt.Println("Moon phase is " + MoonData[pickedMoonData].MoonPhase + " with " + MoonData[pickedMoonData].Visibility + " visibility...")
+	fmt.Println("Picked " + TarotData[pickedTarotData].Name + " tarot card of the " + TarotData[pickedTarotData].Arcana + "...")
 
 }
